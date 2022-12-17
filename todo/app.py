bb8d9a0707558bca963e709fff2528a7db145d1b
Saiberia <80467393+Saiberia@users.noreply.github.com>
2022-12-17 15:40:08 +0300

    Delete app.py

--- todo/app.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from starlette.templating import Jinja2Templates
-
-app = FastAPI()
-templates = Jinja2Templates(directory='todo/templates')
-
-from todo.routes import home
-
