7f9ce5eae42c956ad946040d0a9606aff9a2039b
Saiberia <buai130789@gmail.com>
2022-12-17 14:17:55 +0300

    s

---
+++ todo/config.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+from pydantic import BaseSettings
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    app_name = 'Мой менеджер задач'
+    db_sqlite_url = 'sqlite:///.\\todo\\database\\DB\\todo.db'
+
+
+settings = Settings()
